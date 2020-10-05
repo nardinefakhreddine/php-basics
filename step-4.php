30e0183da5ac8ecdc1ccb5cbed3d67e79e7fd405
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 14:44:25 +0300

    step-4

---
+++ step-4.php
@@ -0,0 +1,9 @@
+<?php
+$name="nardine";
+$position="consultant";
+$githup_url="https://github.com/gk-git";
+echo "Hello, I'm ".$name.", I'm a tech ".$position." please check my github link". $githup_url."<br>";
+echo "Hello, I'm $name , I'm a tech $position please check my github link $githup_url <br>";
+echo 'Hello, I\'m ' .$name .',I\'m a tech ' .$position.'please check my github link '.$githup_url.'<br>';
+
+?>
