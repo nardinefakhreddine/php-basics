157d8aa1c8226c98ea8703029442e9974d4aa8b0
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 15:14:51 +0300

    step-8-a

---
+++ step-8-a.php
@@ -0,0 +1,7 @@
+<?php
+$food = array(" Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+echo "Hello Sir, do you have".$food[0].", ".$food[1].", and Cheese? Also if you sell fruits can I find a".$food[5].
+
+"?";
+?>
