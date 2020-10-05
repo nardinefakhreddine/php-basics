26f57a44655e9c889519190e45f5978a8fd24826
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 16:45:07 +0300

    step-5

---
+++ basic-2/step-5.php
@@ -0,0 +1,41 @@
+<?php 
+function digit($i){
+switch ($i) {
+    case "zero":
+        echo "i equals 0";
+        break;
+    case "one":
+        echo "i equals 1";
+        break;
+    case "two":
+        echo "i equals 2";
+        break;
+       
+            case "three":
+                echo "i equals 3";
+                break;
+            case "four":
+                echo "i equals 4";
+                break;
+            case "five":
+                echo "i equals 5";
+                break;
+                case "six":
+                    echo "i equals 6";
+                    break;
+                case "seven":
+                    echo "i equals 7";
+                    break;
+                case "eight":
+                    echo "i equals 8";
+                    break;
+                    case "nine":
+                        echo "i equals 9";
+                        break;
+}
+}
+$num="three";
+digit($num);
+?>
+
+
