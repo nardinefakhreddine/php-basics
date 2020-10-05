7de49a1237ddaf6aa0c453c918e57f0a4d426dd2
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 14:59:31 +0300

    step-6

---
+++ step-6.php
@@ -0,0 +1,11 @@
+<?php 
+$birth_year=1995;
+$current_year=2020;
+$res=$current_year-$birth_year;
+if($res>18){
+echo "you are a greater You can drive";
+}else{
+    echo "You still a kid, go and play GTA";
+
+}
+?>
