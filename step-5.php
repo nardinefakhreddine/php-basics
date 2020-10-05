8333b1da138e909c4d92b80d390f709818d17dff
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 14:54:57 +0300

    step-5

---
+++ step-5.php
@@ -0,0 +1,18 @@
+<?php 
+$course_name="php";
+
+$enrolled_students=3;
+
+$price=1.1;
+
+$on_discount=true;
+
+echo "Course title: $course_name, the language of no bugs <br>";
+
+echo "Enrolled Students: $enrolled_students  <br>";
+
+echo "Course price: ".$price  ."$ USD <br>";
+ echo "Course on discount:$on_discount";
+
+
+?>
