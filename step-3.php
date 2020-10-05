735b925411a1c3253be9597ee5c063d653d562c8
Nardine <nardinefakhreddine@gmail.com>
2020-10-05 14:24:31 +0300

    step-3

---
+++ step-3.php
@@ -0,0 +1,11 @@
+<?php 
+$name='Nardine';
+$email='nardinefakhreddine@gmail.com';
+echo  "My name is $name and my email is $email.";
+echo "<br>";
+$width=47;
+$height=136;
+$area=$width*$height;
+echo $area;
+
+?>
