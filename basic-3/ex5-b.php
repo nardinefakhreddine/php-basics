1a5c86065627a5e137cd251ea0b0a986487a218f
Nardine <nardinefakhreddine@gmail.com>
2020-10-06 10:33:13 +0300

    ex5-b

--- basic-3/ex5-b.php
+++ basic-3/ex5-b.php
@@ -1,8 +1,8 @@
 <?php  
-$string = "text text reverse";  
+$text = "text text reverse";  
 $length = strlen($string);  
 for ($i=($length-1) ; $i >= 0 ; $i--)   
 {  
-  echo $string[$i];  
+  echo $text[$i];  
 }  
 ?>  
